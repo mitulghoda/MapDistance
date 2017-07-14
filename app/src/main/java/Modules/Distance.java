c7202be0bc469ac4f085c47c5a741b61dd4497d8
mdghoda <md.ghoda@gmail.com>
2017-07-14 15:32:31 +0530

    Map_distance

---
+++ app/src/main/java/Modules/Distance.java
@@ -0,0 +1,14 @@
+package Modules;
+
+/**
+ * Created by Mitul
+ */
+public class Distance {
+    public String text;
+    public int value;
+
+    public Distance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/Modules/Duration.java
@@ -0,0 +1,14 @@
+package Modules;
+
+/**
+ * Created by Mitul
+ */
+public class Duration {
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
